adee94ed800430bf22f3c0f19f29aa429f61ca0b
Abubaker <Abubakersalah4@gmail.com>
2025-01-22 17:38:07 +0100

    added grid && detailed page

--- src/FetchData.jsx
+++ src/FetchData.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import DetailPage from "./DetailPage";
 
-export default function FetchData() {
+export default function FetchData(country) {
   const [countries, setCountries] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [selectedCountry,setSelectedCountry] = useState(null)
 
   useEffect(() => {
     const fetchCountries = async () => {
@@ -24,23 +26,77 @@ export default function FetchData() {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
+  if(selectedCountry) {
+      return(
+            <>
+            <div className="p-4">
+                  <button
+                  onClick={()=> setSelectedCountry(null)} 
+                  className="bg-gray-200 px-3">
+                        back
+                  </button>
+            </div>
+
+          <div className="border h-full m-4 rounded-lg bg-white shadow-md overflow-hidden flex flex-col">
+          <div className="w-full h-[200px]">
+            <img
+              src={selectedCountry.flags.svg || selectedCountry.flags.png}
+              alt={`${selectedCountry.name.common} flag`}
+              className="w-[400px] h-full object-cover"
+            />
+          </div>
+
+          <div className="p-4 w-full font-sans">
+            <h1 className="text-2xl font-semibold text-gray-800">{selectedCountry.name.common}</h1>
+            <p className="text-sm text-gray-600">
+              <span className="font-bold">Population:</span> {selectedCountry.population.toLocaleString()}
+            </p>
+            <p className="text-sm text-gray-600">
+            <span className="font-bold">Region:</span> {selectedCountry.region}
+            </p>
+            <p className="text-sm text-gray-600">
+            <span className="font-bold">Capital:</span>{selectedCountry.capital}
+            </p>
+          </div>
+          </div>
+
+          
+            </>
+      )
+  }
+
+
   return (
-    <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
+      <div
+       className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 p-8">
       {countries.map((country, index) => (
-        <div className="flex flex-col max-w-fit h-72 text-end items-start border rounded-lg shadow-md gap-4 bg-white">
-          <div
-            key={index}
-            className=""
-          >
+        <div
+        onClick={ () => setSelectedCountry(country)}
+          key={index}
+          className="border h-[350px] rounded-lg bg-white shadow-md overflow-hidden flex flex-col"
+        >
+
+          <div className="w-full h-[200px]">
             <img
               src={country.flags.svg || country.flags.png}
               alt={`${country.name.common} flag`}
-              className="w-64 h-36 object-cover rounded-md"
+              className="w-full h-full object-cover"
             />
           </div>
-          <div className="p-4">
-            <h1 className=" text-gray-800">{country.name.common}</h1>
+
+          <div className="p-4 w-full font-sans">
+            <h1 className="text-2xl font-semibold text-gray-800">{country.name.common}</h1>
+            <p className="text-sm text-gray-600">
+              <span className="font-bold">Population:</span> {country.population.toLocaleString()}
+            </p>
+            <p className="text-sm text-gray-600">
+            <span className="font-bold">Region:</span> {country.region}
+            </p>
+            <p className="text-sm text-gray-600">
+            <span className="font-bold">Capital:</span>{country.capital}
+            </p>
           </div>
+          
         </div>
       ))}
     </div>
