771a82e767aa5982d46c71be20e8db5731639353
Abubaker <Abubakersalah4@gmail.com>
2025-01-23 14:17:18 +0100

    removed humburger menue

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,13 @@ import React from "react"
 import Header from "./Header"
 import SearchInput from "./SearchInput"
 import FetchData from "./fetchData"
-function App() {
+export default function App() {
 
   return (
     <>
       < Header />
       < SearchInput />
       < FetchData />
-      
-    
     </>
   )
 }
-
-export default App
