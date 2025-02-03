597710d7b5c8e6cd868c321b244f0e255d770398
Abubaker <Abubakersalah4@gmail.com>
2025-02-03 05:16:34 +0100

    add dark && light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import FetchData from "./FetchData";
 export default function App() {
   const [selectedCountry, setSelectedCountry] = useState(null);
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem("theme") === "dark";
+  });
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkMode]);
+
 
   return (
     <>
-      <Header selectedCountry={selectedCountry} />
+      <Header selectedCountry={selectedCountry} 
+      setDarkMode={setDarkMode}
+      darkMode={darkMode}
+      />
       <FetchData
         setSelectedCountry={setSelectedCountry}
         selectedCountry={selectedCountry}
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 
-export default function Header({selectedCountry}) {
+export default function Header({setDarkMode, darkMode}) {
+  
   return (
-    <>
-      <div className="bg-[#fafafa] text-[#111827] shadow-md py-10 px-6 flex justify-between font-sans">
+    <div className={`bg-white dark:bg-[#1f2937] text-[#111827] dark:text-white shadow-md py-10 px-6 flex justify-between font-sans`}>
+      <h1 className="font-semibold text-lg">Where in the world?</h1>
 
-        <h1 className="font-semibold text-lg">Where in the world?</h1>
-
-        <div className="flex items-center gap-2 text-lg">
-          <ion-icon name="moon-outline"></ion-icon>
-          <button className="font-light">Dark Mode</button>
-        </div>
+      <div className="flex items-center gap-2 text-lg cursor-pointer" onClick={() => setDarkMode(!darkMode)}>
+        <ion-icon name={darkMode ? "moon" : "moon-outline"}></ion-icon>
+        <button className="font-light">{darkMode ? "Light Mode" : "Dark Mode"}</button>
       </div>
-    </>
+    </div>
   );
 }
