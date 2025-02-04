485fab33a944a3b4bb9e967b3726201b11409595
Abubaker <Abubakersalah4@gmail.com>
2025-02-04 02:39:34 +0100

    updated bg color and fixed the inputs

--- src/FetchData.jsx
+++ src/FetchData.jsx
@@ -129,15 +129,15 @@ export default function FetchData({ selectedCountry, setSelectedCountry }) {
 
   return (
     <>
-      <div className="flex flex-col px-10 py-10">
-        <div className="relative w-full md:max-w-80  text-[#111827]">
+      <div className="flex flex-col lg:flex-row  lg:justify-between px-10 lg:px-16 py-10">
+        <div className="relative w-full lg:max-w-[35%]  text-[#111827]">
           <div className="mt-7 absolute mx-16 text-4xl text-[#c5c5c5]">
             <ion-icon name="search-outline"></ion-icon>
           </div>
           <input
             type="text"
             placeholder="Search for a country..."
-            className="pl-32 text-lg py-8 mb-12 w-full rounded-md shadow-md placeholder-[#c5c5c5]"
+            className="pl-32 text-lg py-8 mb-12 w-full dark:bg-[#253341] dark:text-white rounded-md shadow-md placeholder-[#c5c5c5]"
             value={searchValue}
             onChange={handleInputChange}
             aria-label="Search for a country"
@@ -147,7 +147,7 @@ export default function FetchData({ selectedCountry, setSelectedCountry }) {
           <select
             value={selectedRegion}
             onChange={handleRegionChange}
-            className="block w-full text-[#111827] bg-white py-8 px-4 border border-none rounded-md shadow-md placeholder-[#c5c5c5]"
+            className="block w-full text-[#111827] dark:bg-[#253341] dark:text-white bg-white py-8 px-4 border border-none rounded-md shadow-md placeholder-[#c5c5c5]"
             aria-label="Filter countries by region"
           >
             <option value="">All Regions</option>
@@ -161,13 +161,13 @@ export default function FetchData({ selectedCountry, setSelectedCountry }) {
           </select>
         </div>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 p-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 lg:px-16 px-14">
         {filteredCountries.length > 0 ? (
           filteredCountries.map((country) => (
             <div
               onClick={() => setSelectedCountry(country)}
               key={country.cca3}
-              className="border h-[440px] rounded-lg dark:border-gray-600 bg-white dark:bg-[#1f2937] text-[#111827] dark:text-white shadow-md overflow-hidden flex flex-col cursor-pointer"
+              className="border h-[440px] rounded-lg dark:border-none bg-white dark:bg-[#253341] text-[#111827] dark:text-white shadow-md overflow-hidden flex flex-col cursor-pointer"
             >
               <img
                 src={country.flags?.svg || country.flags?.png}
